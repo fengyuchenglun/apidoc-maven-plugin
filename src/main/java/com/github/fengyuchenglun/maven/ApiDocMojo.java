ffa51afa7a9473b3fde2aa24e0b3a88b77e43c37
duanledexianxianxian <fengyuchenglun@foxmail.com>
2020-10-16 16:41:02 +0800

    add:添加单元测试

--- src/main/java/com/github/fengyuchenglun/maven/ApiDocMojo.java
+++ src/main/java/com/github/fengyuchenglun/maven/ApiDocMojo.java
@@ -3,46 +3,122 @@ package com.github.fengyuchenglun.maven;
 
 import com.github.fengyuchenglun.apidoc.core.ApiDoc;
 import com.github.fengyuchenglun.apidoc.core.Context;
+import com.github.fengyuchenglun.apidoc.core.common.enums.FieldShowWay;
+import com.github.fengyuchenglun.apidoc.springmvc.SpringMvcContext;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+
 
 /**
- * generate rest doc with apigcc
+ * generate rest doc with apiDoc
+ *
+ * @author duanledexianxianxian
  */
-@Mojo(name = "build")
+@Mojo(name = "apidoc", requiresDependencyResolution = ResolutionScope.COMPILE)
 public class ApiDocMojo extends AbstractMojo {
 
+    private static final String PROJECT = "project";
+
+    private static final String COMMA = ",";
+    /**
+     * The Project.
+     */
     MavenProject project;
 
+    /**
+     * The Id.
+     */
     @Parameter
     String id;
+    /**
+     * The Name.
+     */
     @Parameter
     String name;
+    /**
+     * The Description.
+     */
     @Parameter
     String description;
+    /**
+     * The Build.
+     */
     @Parameter
     String build;
-    //传字符串，使用逗号分隔
+    /**
+     * The Source.
+     * 传字符串，使用逗号分隔
+     */
     @Parameter
     String source;
+    /**
+     * The Dependency.
+     */
     @Parameter
     String dependency;
+    /**
+     * The Jar.
+     */
     @Parameter
     String jar;
+    /**
+     * The Version.
+     */
     @Parameter
     String version;
+    /**
+     * The Css.
+     */
     @Parameter
     String css;
 
+    /**
+     * 字段显示方式
+     * FLAT-平级
+     * TREE=树级
+     */
+    @Parameter
+    FieldShowWay fieldShowWay;
+
+    /**
+     * 控制器名称
+     */
+    @Parameter
+    String controller;
+    /**
+     * rest控制器名称
+     */
+    @Parameter
+    String restController;
+
+    /**
+     * markdown模版文件路径
+     */
+    @Parameter
+    String template;
+    @Parameter
+    String includes;
+    @Parameter
+    String excludes;
+    @Parameter
+    String pageClassNames;
+    @Parameter
+    String scanCode;
+    @Parameter
+    String scanPackages;
+
+
     @Override
     public void execute() {
-        if (getPluginContext().containsKey("project") && getPluginContext().get("project") instanceof MavenProject) {
-            project = (MavenProject) getPluginContext().get("project");
+        if (getPluginContext().containsKey(PROJECT) && getPluginContext().get(PROJECT) instanceof MavenProject) {
+            project = (MavenProject) getPluginContext().get(PROJECT);
             build();
         }
     }
@@ -50,14 +126,14 @@ public class ApiDocMojo extends AbstractMojo {
     private void build() {
         Context context = new Context();
         if (source != null) {
-            for (String dir : source.split(",")) {
+            for (String dir : source.split(COMMA)) {
                 context.addSource(abs(dir));
             }
         } else {
             context.addSource(project.getBasedir().toPath());
         }
         if (dependency != null) {
-            for (String dir : dependency.split(",")) {
+            for (String dir : dependency.split(COMMA)) {
                 context.addDependency(abs(dir));
             }
         } else {
@@ -65,7 +141,7 @@ public class ApiDocMojo extends AbstractMojo {
             context.addDependency(parent.getBasedir().toPath());
         }
         if (jar != null) {
-            for (String dir : jar.split(",")) {
+            for (String dir : jar.split(COMMA)) {
                 context.addJar(abs(dir));
             }
         }
@@ -93,11 +169,47 @@ public class ApiDocMojo extends AbstractMojo {
         if (css != null) {
             context.setCss(css);
         }
+        // 表格列表展现方式,默认平级
+        if (null != fieldShowWay) {
+            context.setFileShowWay(fieldShowWay);
+        }
 
+        if (null != controller) {
+            context.addController(controller);
+        }
+
+        if (null != restController) {
+            context.addRestController(restController);
+        }
+
+        if (null != template) {
+            context.setTemplate(template);
+        }
+
+        if (includes != null) {
+            context.addIncludes(Arrays.asList(includes.split(COMMA)));
+        }
+
+        if (includes != null) {
+            context.addExcludes(Arrays.asList(excludes.split(COMMA)));
+        }
+
+        if (scanPackages != null) {
+            context.addScanPackages(Arrays.asList(scanPackages.split(COMMA)));
+        }
+
+        if (scanPackages != null) {
+            context.addScanPackages(Arrays.asList(scanPackages.split(COMMA)));
+        }
+        if (pageClassNames != null) {
+            context.addPageClassNames(scanPackages.split(COMMA));
+        }
+        if (scanCode != null) {
+            context.setScanCode(Boolean.valueOf(scanCode));
+        }
         ApiDoc apiDoc = new ApiDoc(context);
         apiDoc.parse();
         apiDoc.render();
-
     }
 
     private MavenProject findParent(MavenProject mp) {
@@ -116,82 +228,182 @@ public class ApiDocMojo extends AbstractMojo {
         }
     }
 
+    /**
+     * Gets project.
+     *
+     * @return the project
+     */
     public MavenProject getProject() {
         return project;
     }
 
+    /**
+     * Sets project.
+     *
+     * @param project the project
+     */
     public void setProject(MavenProject project) {
         this.project = project;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets description.
+     *
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets description.
+     *
+     * @param description the description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets build.
+     *
+     * @return the build
+     */
     public String getBuild() {
         return build;
     }
 
+    /**
+     * Sets build.
+     *
+     * @param build the build
+     */
     public void setBuild(String build) {
         this.build = build;
     }
 
+    /**
+     * Gets source.
+     *
+     * @return the source
+     */
     public String getSource() {
         return source;
     }
 
+    /**
+     * Sets source.
+     *
+     * @param source the source
+     */
     public void setSource(String source) {
         this.source = source;
     }
 
+    /**
+     * Gets dependency.
+     *
+     * @return the dependency
+     */
     public String getDependency() {
         return dependency;
     }
 
+    /**
+     * Sets dependency.
+     *
+     * @param dependency the dependency
+     */
     public void setDependency(String dependency) {
         this.dependency = dependency;
     }
 
+    /**
+     * Gets jar.
+     *
+     * @return the jar
+     */
     public String getJar() {
         return jar;
     }
 
+    /**
+     * Sets jar.
+     *
+     * @param jar the jar
+     */
     public void setJar(String jar) {
         this.jar = jar;
     }
 
+    /**
+     * Gets version.
+     *
+     * @return the version
+     */
     public String getVersion() {
         return version;
     }
 
+    /**
+     * Sets version.
+     *
+     * @param version the version
+     */
     public void setVersion(String version) {
         this.version = version;
     }
 
+    /**
+     * Gets css.
+     *
+     * @return the css
+     */
     public String getCss() {
         return css;
     }
 
+    /**
+     * Sets css.
+     *
+     * @param css the css
+     */
     public void setCss(String css) {
         this.css = css;
     }
